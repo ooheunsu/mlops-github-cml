f433912bf72f2b846ef148efd48341ece65ac836
ooheunsu <oes0508@naver.com>
2025-05-14 13:37:22 +0900

    Changed Algorithm: RandomForestClassifier

--- train.py
+++ train.py
@@ -30,10 +30,10 @@ X = imp.transform(X)
 
 # Linear model
 # clf = LogisticRegression()
-yhat = cross_val_predict(clf, X, y, cv=5)
 
 # RandomForest
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
+yhat = cross_val_predict(clf, X, y, cv=5)
 
 acc = np.mean(yhat==y)
 tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
